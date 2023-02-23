45dd57ebd61b652cb8f1f14f6a7566280edeb129
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-02-23 23:35:33 +0100

    wip - add more types

--- api/src/utils/types.ts
+++ api/src/utils/types.ts
@@ -1,11 +1,15 @@
+export type NasdaqListedStock = {
+  companyName: string
+  financialStatus: string
+  marketCategory: string
+  roundLotSize: number
+  securityName: string
+  symbol: string
+  testIssue: string
+}
+
 export type StockPrice = {
   price: string
 }
 
 export type Ticker = string
-
-// export type GetStockPriceRequest = {
-//   params: {
-//     ticker: Ticker
-//   }
-// }
