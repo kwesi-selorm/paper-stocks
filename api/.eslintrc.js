dee800a7d534bd73b41ce71077a7c1d172b306b6
Jeffery Adorkor <jead@v-p-s.com>
2023-02-23 11:53:27 +0100

    wip-install prettier and update styles

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,17 +1,18 @@
-module.exports = {   "globals": {module: true},
-    "env": {
-      "browser": true,
-      "es2021": true,
-      "jest": true,
-        "node":true
-    },
-    "extends":["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
-    "@typescript-eslint"
-    ],
-    "rules": {
-    }
+module.exports = {
+  globals: { module: true },
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {}
 }
--- api/src/app.ts
+++ api/src/app.ts
@@ -1,19 +1,19 @@
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
-import cors from "cors";
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import indexRouter from './routes/index'
+import usersRouter from './routes/users'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
-export default app;
+export default app
