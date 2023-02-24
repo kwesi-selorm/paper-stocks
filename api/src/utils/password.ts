359258822243901f6b3c3b647c71c8a63562810d
Jeffery Adorkor <jead@v-p-s.com>
2023-02-24 11:01:04 +0100

    finish function

--- api/src/utils/password.ts
+++ api/src/utils/password.ts
@@ -9,14 +9,16 @@ export function encodePassword(password: string): string {
   return crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha256').toString('hex')
 }
 
-export function decodePasswordHash(password: string, salt: string): boolean {
+export function decodePasswordHash(
+  password: string,
+  salt: string,
+  dbPasswordHash: string
+): boolean {
   const passwordHash = crypto
     .pbkdf2Sync(password, salt, 10000, 64, 'sha256')
     .toString('hex')
   return crypto.timingSafeEqual(
     Buffer.from(passwordHash),
-    Buffer.from(
-      'bab365130ac91ca24aaa58a729ca2cfd9a67b761187d92e557610c95a7cd4c5747e976eb1f2eddf6ee439e9e186122c467b6449bd965ca7cd97dc83884651d4b'
-    )
+    Buffer.from(dbPasswordHash)
   )
 }
