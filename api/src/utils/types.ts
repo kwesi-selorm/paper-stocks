9043ef3956dce0d695b297150aa0527fcd805e79
Jeffery Adorkor <jead@v-p-s.com>
2023-02-24 18:42:39 +0100

    add new types

--- api/src/utils/types.ts
+++ api/src/utils/types.ts
@@ -1,3 +1,19 @@
+import { Schema } from 'mongoose'
+
+export interface AssetRecord {
+  id: Schema.Types.ObjectId
+  position: number
+  returnNumber: number
+  returnPercent: number
+  lastPrice: number
+  users: [UserRecord]
+}
+
+export type EncodePasswordOutput = {
+  passwordSalt: string
+  passwordHash: string
+}
+
 export type GetStockPriceInput = {
   symbol: string
 }
@@ -10,6 +26,39 @@ export type NasdaqListedStock = {
   securityName: string
   symbol: string
   testIssue: string
+}
+
+export type SaveUserInput = {
+  email: string
+  username: string
+  passwordHash: string
+  passwordSalt: string
+  passwordClue: string
+  assets: AssetRecord[]
+  buyingPower: number
+}
+
+export interface UserRecord {
+  _id: Schema.Types.ObjectId
+  email: string
+  username: string
+  passwordHash: string
+  passwordSalt: string
+  passwordClue: string
+  assets: AssetRecord[]
+  buyingPower: number
+}
+
+export type UserSignInInput = {
+  email: string
+  password: string
+}
+
+export type UserSignUpInput = {
+  email: string
+  username: string
+  password: string
+  passwordClue: string
 }
 
 export type StockPrice = {
