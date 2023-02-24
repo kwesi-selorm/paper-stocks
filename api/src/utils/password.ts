9876c7035cbe7a0bcf457028188a1378f7e6ef82
Jeffery Adorkor <jead@v-p-s.com>
2023-02-24 18:42:28 +0100

    update password encoding logic

--- api/src/utils/password.ts
+++ api/src/utils/password.ts
@@ -1,12 +1,16 @@
 import * as crypto from 'node:crypto'
+import { EncodePasswordOutput } from './types'
 
 export function generatePasswordSalt(): string {
   return crypto.randomBytes(16).toString('hex')
 }
 
-export function encodePassword(password: string): string {
-  const salt = generatePasswordSalt()
-  return crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha256').toString('hex')
+export function encodePassword(password: string): EncodePasswordOutput {
+  const passwordSalt = generatePasswordSalt()
+  const passwordHash = crypto
+    .pbkdf2Sync(password, passwordSalt, 10000, 64, 'sha256')
+    .toString('hex')
+  return { passwordSalt, passwordHash }
 }
 
 export function decodePasswordHash(
