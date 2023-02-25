377f0b21452ef03e1e8d3a8b1a5f6c79dea87139
Jeffery Adorkor <jead@v-p-s.com>
2023-02-25 16:45:15 +0100

    update the user model

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,5 +1,4 @@
 import mongoose, { Schema } from 'mongoose'
-import Asset from './asset'
 
 export interface IUser {
   id: Schema.Types.ObjectId
@@ -8,20 +7,17 @@ export interface IUser {
   passwordHash: string
   passwordSalt: string
   passwordClue: string
-  assets: Schema.Types.ObjectId[]
   buyingPower: number
 }
 
 const userSchema: Schema = new Schema<IUser>({
-  id: Schema.Types.ObjectId,
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   passwordSalt: { type: String, required: true },
   passwordClue: String,
-  assets: [{ type: Schema.Types.ObjectId, ref: Asset }],
   buyingPower: { type: Number, required: true }
 })
 
-const UserModel = mongoose.model<IUser>('User', userSchema, 'users')
+const UserModel = mongoose.model<IUser>('UserModel', userSchema, 'users')
 export default UserModel
