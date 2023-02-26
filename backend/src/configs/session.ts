97db416b3aa529df129e368c3902940651a30d0a
Jeffery Adorkor <jead@v-p-s.com>
2023-02-26 23:45:39 +0100

    update session config to use prod database

--- backend/src/configs/session.ts
+++ backend/src/configs/session.ts
@@ -2,6 +2,10 @@ import MongoStore from 'connect-mongo'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+if (process.env.MONGODB_URL_PROD === undefined) {
+  throw new Error('The MongoDB connection string is not defined')
+}
+
 const sessionConfigOptions = {
   secret: 'session-secret',
   resave: false,
@@ -10,7 +14,7 @@ const sessionConfigOptions = {
     maxAge: 1000 * 60 * 60 * 24 * 7
   },
   store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URL,
+    mongoUrl: process.env.MONGODB_URL_PROD,
     touchAfter: 24 * 60 * 60, // Interval (in seconds) between session updates
     autoRemove: 'native', // Remove sessions when they expire
     ttl: 1000 * 60 * 60 * 24 * 7, // 1 week
