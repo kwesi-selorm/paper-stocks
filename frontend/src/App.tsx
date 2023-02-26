ccd28024cc451805f1009a81758526033d075d13
Jeffery Adorkor <jead@v-p-s.com>
2023-02-26 19:41:22 +0100

    frontend setup with vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import React from "react";
+import "./styles/App.css";
+import { DatePicker } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Paper Stocks</h1>
+    </div>
+  );
+}
+
+export default App;
