188ca91dc41fda81933ada050f47dccd49715f4a
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-02-28 23:53:58 +0100

    wip - additional types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,4 +1,18 @@
-export type SigninInput = {
+export type SignInInput = {
   username: string
   password: string
+}
+
+export type UserParam = {
+  userId: string
+}
+
+export type Asset = {
+  _id: string
+  symbol: string
+  name: string
+  position: number
+  averagePrice: number
+  userId: string
+  __v: number
 }
