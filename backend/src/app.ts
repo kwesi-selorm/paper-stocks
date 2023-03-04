5933baf2479634d30a423da98b4a0ca72f4258e3
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-03-05 00:50:46 +0100

    add cors config to allow requests from localhost

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,12 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  })
+)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
