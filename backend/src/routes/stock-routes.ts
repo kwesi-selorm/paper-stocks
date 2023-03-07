94a9177c21dded4aed0d6c92335a125f7e17a691
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-03-08 00:27:25 +0100

    update function to fetch the ticker stock prices

--- backend/src/routes/stock-routes.ts
+++ backend/src/routes/stock-routes.ts
@@ -1,15 +1,10 @@
 import express from 'express'
 import getStockPrice from '../controllers/stocks/get-stock-price'
 import getListedStocks from '../controllers/stocks/get-listed-stocks'
-import validateGetStockPriceInput from '../validators/stocks/get-stock-price-input-validator'
 
 const stocksRouter = express.Router()
 
-stocksRouter.get(
-  '/stock-price/:symbol',
-  validateGetStockPriceInput,
-  getStockPrice
-)
+stocksRouter.get('/stock-price/:userId', getStockPrice)
 
 stocksRouter.get('/nasdaq/all', getListedStocks)
 
