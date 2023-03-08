49e8d06afa9736593e866109a413f08941f91fbc
Jeffery Adorkor <jead@v-p-s.com>
2023-03-08 19:47:14 +0100

    complete buying and selling calculation functions

--- backend/src/helpers/asset-calculations.ts
+++ backend/src/helpers/asset-calculations.ts
@@ -1,4 +1,4 @@
-export function calculatePositionAndAverage(
+export function findPositionAndAverageOnBuy(
   currentPosition: number,
   currentAmountInvested: number,
   newPosition: number,
@@ -8,4 +8,17 @@ export function calculatePositionAndAverage(
   const totalAmountInvested = currentAmountInvested + newAmountInvested
   const averagePrice = totalAmountInvested / totalPosition
   return { totalPosition, averagePrice }
+}
+
+export function findPositionAndAverageOnSell(
+  currentPosition: number,
+  currentAveragePrice: number,
+  positionsSold: number,
+  unitPrice: number
+) {
+  const costBasis = positionsSold * currentAveragePrice
+  const netProceeds = positionsSold * unitPrice
+  const gainOrLoss = netProceeds - costBasis
+  const remainingPosition = currentPosition - positionsSold
+  return { remainingPosition, gainOrLoss }
 }
