2fbe06b23fe963fef9dfc52ec3b6a09fd04f3c7c
Jeffery Adorkor <jead@v-p-s.com>
2023-03-08 19:46:54 +0100

    add sell asset controller to route and update validator schema

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -4,11 +4,13 @@ import signIn from '../controllers/users/sign-in'
 import buyAsset from '../controllers/users/buy-asset'
 import getAssets from '../controllers/users/get-assets'
 import { generateToken, verifyToken } from '../middleware/jwt-auth'
+import sellAsset from '../controllers/users/sell-asset'
 const usersRouter = express.Router()
 
 usersRouter.post('/signup', generateToken, signUp)
 usersRouter.post('/signin', generateToken, signIn)
 usersRouter.post('/buy-asset/:userId', verifyToken, buyAsset)
+usersRouter.post('/sell-asset/:userId', verifyToken, sellAsset)
 usersRouter.get('/get-assets/:userId', verifyToken, getAssets)
 
 export default usersRouter
