c24c3f02724e8fcf54a0640d4bafe7dd98ef8920
Jeffery Adorkor <jead@v-p-s.com>
2023-03-08 10:27:09 +0100

    signup function

---
+++ frontend/src/api/signup.ts
@@ -0,0 +1,21 @@
+import axios from "axios"
+import { apiUrl } from "@/utils/constants"
+import { SignUpInput } from "@/utils/types"
+
+export default async function signUp(input: SignUpInput) {
+  const axiosConfig = {
+    method: "POST",
+    url: `${apiUrl}/users/signup`,
+    data: {
+      username: input.username,
+      email: input.username,
+      password: input.password,
+      confirmPassword: input.confirmPassword,
+      passwordClue: input.passwordClue
+    } as SignUpInput,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+  return await axios.request(axiosConfig)
+}
