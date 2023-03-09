09b5d6b0277a015bbd5bce5ba63f215aa1d6536d
Jeffery Adorkor <jead@v-p-s.com>
2023-03-09 03:03:18 +0100

    create modals and the function to render them

--- frontend/src/components/BuySellAssetModal.tsx
+++ frontend/src/components/BuySellAssetModal.tsx
@@ -4,6 +4,8 @@ import getListedStocks from "@/api/get-listed-stocks"
 import { ListedStock } from "@/utils/types"
 import { useQuery } from "react-query"
 import { AxiosError } from "axios"
+import BuyAssetModal from "@/components/modals/BuyAssetModal"
+import BuyNewStockModal from "@/components/modals/BuyNewStockModal"
 
 type ModalProps = {
   modalOpen: boolean
@@ -11,45 +13,13 @@ type ModalProps = {
   transactionType: string
 }
 
-type ContentProps = {
-  listedStocks: Array<ListedStock>
-}
-
 const BuySellAssetModal: React.FC<ModalProps> = ({
   modalOpen,
   setModalOpen,
   transactionType
 }) => {
-  const [listedStocks, setListedStocks] = useState<Array<ListedStock>>([])
-  const { error, isLoading, isError } = useQuery(
-    ["listedStocks"],
-    getListedStocks,
-    {
-      retry: false,
-      retryOnMount: false,
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      onSuccess: (data) => {
-        if (data) {
-          setListedStocks(data.listedStocks)
-        }
-      }
-    }
-  )
-
   const handleCancel = () => {
     setModalOpen(false)
-  }
-
-  if (isLoading) {
-    return <Spin />
-  }
-
-  if (isError) {
-    if (error instanceof AxiosError) {
-      message.error(error?.response?.data.message).then()
-    }
-    message.error(JSON.stringify(error)).then()
   }
 
   return (
@@ -65,34 +35,7 @@ const BuySellAssetModal: React.FC<ModalProps> = ({
       okText={transactionType === "buy" ? "Buy" : "Sell"}
       open={modalOpen}
       onCancel={handleCancel}
-    >
-      <ModalContent listedStocks={listedStocks} />
-    </Modal>
-  )
-}
-
-const ModalContent: React.FC<ContentProps> = ({ listedStocks }) => {
-  const stockOptions = listedStocks.map((listedStock) => {
-    return {
-      value: listedStock.symbol,
-      label: listedStock.name
-    }
-  })
-
-  return (
-    <Form>
-      <Select
-        allowClear
-        bordered
-        placeholder="Select a stock"
-        options={stockOptions}
-        filterOption={(input, option) =>
-          (option?.label.toLowerCase() ?? "").includes(input.toLowerCase())
-        }
-        showSearch
-        style={{ width: 250 }}
-      />
-    </Form>
+    ></Modal>
   )
 }
 
