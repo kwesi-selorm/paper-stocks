b065ab77605f829de1b844f6ca1998069bb8f95e
Jeffery Adorkor <jead@v-p-s.com>
2023-03-09 10:55:22 +0100

    add constant

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,3 +2,10 @@ export const apiUrl =
   process.env.NODE_ENV == "development"
     ? process.env.LOCALHOST_URL ?? "http://localhost:8000/api"
     : process.env.HEROKU_URL
+
+export const newStockInitialInput = {
+  symbol: "",
+  name: "",
+  position: 0,
+  amountInvested: 0
+}
