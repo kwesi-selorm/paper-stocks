02eae0064054cbfd757aad50ec46164ffc23502a
Jeffery Adorkor <jead@v-p-s.com>
2023-03-10 14:15:12 +0100

    move function to calculate percentage return

--- frontend/src/utils/number-utils.ts
+++ frontend/src/utils/number-utils.ts
@@ -1,14 +1,3 @@
 export function formatToCurrencyString(value: number): string {
   return value.toLocaleString("en-US", { style: "currency", currency: "USD" })
 }
-
-export function findPercentageReturn(
-  purchasingPower: number,
-  netAssetValue: number
-) {
-  const cashSpent = 100000 - purchasingPower
-  if (cashSpent === netAssetValue) {
-    return 0
-  }
-  return ((netAssetValue - cashSpent) / cashSpent) * 100
-}
